040d87456cd41e65a4e8689d596d65064fc98b10
andrewbaughman <52512098+andrewbaughman@users.noreply.github.com>
2020-02-19 17:22:12 -0500

    added home and hide tabs feature

---
+++ Cole/javascript.js
@@ -0,0 +1,10 @@
+// JavaScript Document
+
+function NavHide() {
+	var x = document.getElementById("NavEmployee");
+	if (x.style.display === "none") {
+		x.style.display = "block";
+	} else {
+		x.style.display = "none";
+	}
+}
