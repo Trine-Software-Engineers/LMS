f14d81a022f1102a4d5eb7bcf555f66b8a8e982a
ClaytonWilson12 <54342029+ClaytonWilson12@users.noreply.github.com>
2020-02-26 23:31:11 -0500

    commit

--- editbook.php
+++ editbook.php
@@ -1,3 +1,48 @@
+<?php
+
+$user = 'root';
+$pass = '';
+$db = 'lms';
+
+$conn = new mysqli('localhost', $user, $pass, $db) or die("unable to connect");
+
+if($conn->connect_error)
+{
+    die("Connection failed");
+}
+
+if(isset($_POST['submit']))
+{
+    $isbn = $_POST['bookISBN'];
+    $newISBN = $_POST['newBookISBN'];
+    $title = $_POST['newBookTitle'];
+    $auth = $_POST['newBookAuthor'];
+	$q = $_POST['newBookQuantity'];
+	$mem = $_POST['newBookMemberLoanTime'];
+	$add = $_POST['newBookAdminLoanTime'];
+
+
+	$sql="DELETE FROM `book` WHERE `book`.`ISBN` = '$isbn'";
+	$conn->query($sql);
+/*
+    $sql="INSERT INTO `book` (`Title`, `Author`, `ISBN`, `memLoanTime`, `adminLoanTime`, `numCopies`) VALUES ('$title', '$auth', '$newISBN', '$mem', '$add', '$q')";
+
+    if($conn->query($sql) == TRUE)
+    {
+        echo "book has been successfully added";
+    }
+    else
+    {
+        echo "Ucard already in use";
+	}
+	*/
+}
+
+$conn->close();
+
+
+?>
+
 <html lang="en" dir="ltr">
   <head>
     <meta charset="utf-8">
@@ -17,6 +62,7 @@
 
 <div class="login-box">
 	  <h1>Edit a Book</h1>
+	  <form method="POST" action="#">
           	<div class="textbox">
 				<input type="text" name="bookISBN" placeholder="Original ISBN">
 	  		</div>
@@ -44,10 +90,8 @@
 			<div class="textbox">
 				<input type="text" name="newBookAdminLoanTime" placeholder="New Admin loan time">
 	  		</div>
-
-
-		
-		  	<input type="submit" class="btn" value="Edit Book">
+		  	<input type="submit" class="btn" type= "submit" value="Edit Book">
+		</form>
 	</div>
 
 	
--- editperson.php
+++ editperson.php
@@ -1,3 +1,46 @@
+<?php
+
+$user = 'root';
+$pass = '';
+$db = 'lms';
+
+$conn = new mysqli('localhost', $user, $pass, $db) or die("unable to connect");
+
+if($conn->connect_error)
+{
+    die("Connection failed");
+}
+
+if(isset($_POST['submit']))
+{
+    $uCard = $_POST['uCard'];
+    $card = $_POST['newPersonUCard'];
+    $name = $_POST['newPersonName'];
+    $add = $_POST['newPersonAddress'];
+	$tele = $_POST['newPersonTelephone'];
+	$type = $_POST['type'];
+
+
+	$sql="DELETE FROM `user` WHERE `user`.`Ucard` = '$uCard'";
+	$conn->query($sql);
+
+    $sql="INSERT INTO `user` (`position`, `Ucard`, `name`, `address`, `telephone`) VALUES ('$type', '$card', '$name', '$add', '$tele')";
+
+    if($conn->query($sql) == TRUE)
+    {
+        echo "Member has been successfully added";
+    }
+    else
+    {
+        echo "Ucard already in use";
+    }
+}
+
+$conn->close();
+
+
+?>
+
 <html lang="en" dir="ltr">
   <head>
     <meta charset="utf-8">
@@ -12,22 +55,13 @@
     </div>
 </div>
 	
-<body>
-
- 
-
-
-
-	
+<body>	
 	<div class="login-box">
 	  <h1>Edit a Person</h1>
+	  <form method="POST" action="#">
           	<div class="textbox">
 				<input type="text" name="uCard" placeholder="Original U-Card">
 	  		</div>
-
-
-
-
 			<div class="textbox">
 				<input type="text" name="newPersonUCard" placeholder="New U-Card">
 	  		</div>
@@ -43,30 +77,18 @@
 			<div class="textbox">
 				<input type="text" name="newPersonTelephone" placeholder="New Telephone">
 	  		</div>
-		
-			  <form action="/action_page.php">
 				<select id="type" name="type">
 					<option value="Member">Member</option>
 					<option value="Manager">Manager</option>
 					<option value="Admin">Admin</option>
-
-
-				</select>
-			</form>
-
-
-		
-		  	<input type="submit" class="btn" value="Edit Person">
+				</select>	
+		  	<input type="submit" class="btn" name= "submit" value="Edit Person">
+		</form>
 	</div>
-
-	
-
-
-
-
 
 </body>
 </html>
+
 <script>
 function goBack() {
   window.history.back();
